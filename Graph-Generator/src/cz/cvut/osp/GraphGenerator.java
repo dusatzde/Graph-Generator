171f0a72eef1b9f27c8925e288415e55a2f0fadc
dusatzde <zdenek.dusatko@gmail.com>
2012-03-06 15:39:02 +0100

    Start Graph-Generator project Signed-off-by: dusatzde <zdenek.dusatko@gmail.com>

---
+++ Graph-Generator/src/cz/cvut/osp/GraphGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.osp;
+
+/**
+ *
+ * @author ZDENEK
+ */
+public class GraphGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
